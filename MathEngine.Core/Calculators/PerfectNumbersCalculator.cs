f7aab761f5fd69f974ccde8368d5e09bb530b49c
Massimo Bonanni <massimo.bonanni@microsoft.com>
2019-04-27 11:45:48 +0200

    Fix su bug

--- MathEngine.Core/Calculators/PerfectNumbersCalculator.cs
+++ MathEngine.Core/Calculators/PerfectNumbersCalculator.cs
@@ -11,44 +11,44 @@ namespace MathEngine.Core.Calculators
     {
         public string Name => "perfectnumbers";
 
-        //private static IDictionary<int, double> Numbers = new Dictionary<int, double>() {
-        //    {1,6.0},
-        //    {2,28.0},
-        //    {3,496-0},
-        //    {4,8128.0},
-        //    {5,33550336.0},
-        //    {6,8589869056.0},
-        //    {7,137438691328.0},
-        //    {8,2305843008139952128.0},
-        //};
-        //public IEnumerable<double> Calculate(double upperLimit)
-        //{
-        //    for (var i = 0; i < upperLimit; i++)
-        //    {
-        //        yield return Numbers[i + 1];
-        //    }
-
-        //}
-
+        private static IDictionary<int, double> Numbers = new Dictionary<int, double>() {
+            {1,6.0},
+            {2,28.0},
+            {3,496-0},
+            {4,8128.0},
+            {5,33550336.0},
+            {6,8589869056.0},
+            {7,137438691328.0},
+            {8,2305843008139952128.0},
+        };
         public IEnumerable<double> Calculate(double upperLimit)
         {
-            if (upperLimit >= 1)
-                yield return 6.0;
-
-            var innerCalculator = new PrimeNumbersCalculator();
-
-            var counter = 2;
-            while (counter <= upperLimit)
+            for (var i = 0; i < upperLimit; i++)
             {
-                var prime = innerCalculator.Calculate(counter + 1).Last();
-                var perfect = Math.Pow(2, prime - 1) * (Math.Pow(2, prime) - 1);
-                var sumDividers = perfect.GetDividers().Aggregate((a, b) => a + b);
-                if (sumDividers == perfect)
-                {
-                    yield return perfect;
-                    counter++;
-                }
+                yield return Numbers[i + 1];
             }
+
         }
+
+        //public IEnumerable<double> Calculate(double upperLimit)
+        //{
+        //    if (upperLimit >= 1)
+        //        yield return 6.0;
+
+        //    var innerCalculator = new PrimeNumbersCalculator();
+
+        //    var counter = 2;
+        //    while (counter <= upperLimit)
+        //    {
+        //        var prime = innerCalculator.Calculate(counter + 1).Last();
+        //        var perfect = Math.Pow(2, prime - 1) * (Math.Pow(2, prime) - 1);
+        //        var sumDividers = perfect.GetDividers().Aggregate((a, b) => a + b);
+        //        if (sumDividers == perfect)
+        //        {
+        //            yield return perfect;
+        //            counter++;
+        //        }
+        //    }
+        //}
     }
 }
